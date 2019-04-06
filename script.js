f26b91edf2cefcd1813968b058122a4b231801c5
com-master <42337050+com-master@users.noreply.github.com>
2019-04-06 22:27:32 +0300

    update

--- script.js
+++ script.js
@@ -1,22 +1,77 @@
-var socket = new WebSocket("ws://127.0.0.1:9001");
-socket.onopen = function() {
-  alert("Соединение установлено.");
-};
+//////////////////socket//////////////////
 
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    alert('Соединение закрыто чисто');
-  } else {
-    alert('Обрыв соединения'); // например, "убит" процесс сервера
-  }
-  alert('Код: ' + event.code + ' причина: ' + event.reason);
-};
+// var socket = new WebSocket("ws://127.0.0.1:9001");
+// socket.onopen = function() {
+//   alert("Соединение установлено.");
+// };
 
-socket.onmessage = function(event) {
-  alert("Получены данные " + event.data);
-};
+// socket.onclose = function(event) {
+//   if (event.wasClean) {
+//     alert('Соединение закрыто чисто');
+//   } else {
+//     alert('Обрыв соединения'); // например, "убит" процесс сервера
+//   }
+//   alert('Код: ' + event.code + ' причина: ' + event.reason);
+// };
 
-socket.onerror = function(error) {
-  alert("Ошибка " + error.message);
-};
-socket.send("Привет");
+// socket.onmessage = function(event) {
+//   alert("Получены данные " + event.data);
+// };
+
+// socket.onerror = function(error) {
+//   alert("Ошибка " + error.message);
+// };
+// socket.send("Привет");
+
+//////////////////game//////////////////
+var cord1 = 1
+var KEY_W = 87; 
+var KEY_A = 65;
+var KEY_S = 83;
+var KEY_D = 68;
+document.onkeydown = function(e) 
+{
+    if (e.keyCode == KEY_W) 
+	    {
+	    	if (cord1 > 41) 
+	    	{
+	    		document.getElementById('game' + cord1).style.backgroundColor = 'white';
+	    		document.getElementById('game' + cord1).style.backgroundImage = 'url("")'; 
+	    		cord1 = cord1 - 41
+	    		document.getElementById('game' + cord1).style.backgroundImage = 'url("tank.png")'; 
+	    	}
+	    }
+     if (e.keyCode == KEY_A)
+	    {
+	    	if ((cord1 % 41) != 1)
+	    	{
+	    		document.getElementById('game' + cord1).style.backgroundColor = 'white';
+	    		document.getElementById('game' + cord1).style.backgroundImage = 'url("")'; 
+	    		
+	    		cord1 = cord1 - 1
+	    		document.getElementById('game' + cord1).style.backgroundImage = 'url("tank3.png")'; 
+
+	    	}
+	    }
+    if (e.keyCode == KEY_S) 
+	    {
+	    	if (cord1 + 41 < 1066) 
+	    	{
+	    		//socket.send(parseInt(rand1) + 12)
+	    		document.getElementById('game' + cord1).style.backgroundColor = 'white';
+	    		document.getElementById('game' + cord1).style.backgroundImage = 'url("")'; 
+	    		cord1 = cord1 + 41
+	    		document.getElementById('game' + cord1).style.backgroundImage = 'url("tank2.png")'; 
+	    	}
+	    }
+    if (e.keyCode == KEY_D) 
+	    {
+	    	if ((cord1 % 41) != 0)
+		    	{
+		    		document.getElementById('game' + cord1).style.backgroundColor = 'white';
+		    		document.getElementById('game' + cord1).style.backgroundImage = 'url("")'; 
+		    		cord1 = cord1 + 1
+		    		document.getElementById('game' + cord1).style.backgroundImage = 'url("tank1.png")'; 
+		    	}
+	    }
+}
